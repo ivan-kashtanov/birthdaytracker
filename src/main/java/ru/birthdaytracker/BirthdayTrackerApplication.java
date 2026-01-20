b9f825bab53c99bdcaa605a6b82edabf6b2e3206
ivan-kashtanov <iwan200441@gmail.com>
2026-01-20 23:57:49 +0300

    first commit

---
+++ src/main/java/ru/birthdaytracker/BirthdayTrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.birthdaytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayTrackerApplication.class, args);
+	}
+
+}
